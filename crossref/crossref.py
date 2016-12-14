f8b75b284fe5f46d4156c7d9fda2e51361f8fbf0
Jackson Masache Romero <jgmasache@utpl.edu.ec>
2016-12-14 14:43:34 -0500

    Crossref

---
+++ crossref/crossref.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import orcid
+
+doi="10.1109/EDUCON.2011.5773299"
+url= requests.get("http://api.crossref.org/works/"+doi)
+if url.status_code==200:
+    print "completo"
+    data=json.loads(url.text)
+    member=data['message']['member']
+    print "DOI:"+ data['message']['DOI']
+    print "URL:"+ data['message']['URL']
+    print "Miembro "+member.lstrip("http://id.crossref.org/member/"+"\n")
+    member=member.lstrip("http://id.crossref.org/member/")
+
+
+    url_member= requests.get("http://api.crossref.org/members/"+member)
+    data_member=json.loads(url_member.text)
+    print "Informacion member\n"
+    print "Nombre: " + data_member['message']['primary-name']
+    print "Locacion: "+data_member['message']['location']
+
+authors = orcid.search('john wilbanks')
+print next(authors).family_name
+    
